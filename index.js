6bdf87c7d9b5403ef004e6a1a23f68dea1ca5cb6
Michele Salvini <michele@scozzese.com>
2017-09-22 12:50:46 +0200

    fullPath FIX

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = function (json, map) {
 
-  function resolve (path, rootObj, originalPath) {
+  function resolve (path, rootObj, fullPath) {
 
     var obj = rootObj,
+        fullPath = fullPath || path,
         a = path.split('.');
 
     for (var i = 0, n = a.length-1; i < n; ++i) {
       var k = a.shift();
       if (k == '[]' && obj.length)
-        return obj.map(function (v) { resolve(a.join('.'), v, originalPath || path); });
+        return obj.map(function (v) { resolve(a.join('.'), v, fullPath); });
 
       if (k in obj && obj[k]) {
         obj = obj[k];
@@ -23,8 +24,8 @@ module.exports = function (json, map) {
     var k = a.pop(),
         value = obj[k];
 
-    if (map[originalPath])
-      obj[k] = map[originalPath](value, path, obj);
+    if (map[fullPath])
+      obj[k] = map[fullPath](value, fullPath, obj);
   }
 
   Object.keys(map).map(function (path) {
